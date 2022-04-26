2b7e50c69b44acc2d18e41bbec419b7aae7d5ba7
“moHam <mohamgoham@gmail.com>
2022-04-26 07:54:17 -0400

    test scenario1

--- docker/e2e/chaos.sh
+++ docker/e2e/chaos.sh
@@ -1,22 +1,41 @@
-#!/usr/bin/env bash
+#!/bin/bash 
+RED='\033[0;31m'
+NC='\033[0m'
+CYAN='\033[0;36m'
+c=1
+fun1 () {
+    echo -e "${RED}RUN TENDERMINT CHAOS TEST 1${RED}"
+    while [ $c -le 15 ]
+    do
+    echo "${RED}STOP TENDERMINT CHAOS TEST $c times${RED}"
+    ((c++))
+    docker stop e2e-tendermint-1-1 
+	  sleep 2
+        echo "${CYAN}START TENDERMINT NODE 1  $c times${CYAN}"
 
-pause_node1() {
-  echo "Pausing node1"
-  docker stop e2e-tendermint-1-1
+	  docker start e2e-tendermint-1-1
+    sleep 4
+
+    docker stop e2e-tendermint-0-1 
+	  sleep 3
+        echo "${CYAN}START TENDERMINT NODE 0  $c times${CYAN}"
+    docker start e2e-tendermint-0-1 
+	  sleep 3
+
+        docker stop e2e-tendermint-3-1 
+	  sleep 1
+        echo "${CYAN}START TENDERMINT NODE 3  $c times${CYAN}"
+    docker start e2e-tendermint-3-1 
+	  sleep 1
+
+        echo "${CYAN}START & STOP TENDERMINT NODE 2  $c times${CYAN}"
+       docker stop e2e-tendermint-2-1 
+	  sleep 3
+    docker start e2e-tendermint-2-1 
+	  sleep 3
+
+  done
+    [[ "@" ]] && echo "options: $@"
 }
 
-#stop docker container tendermint-2-1 and restart it after 10s
-pause_node2() {
-  echo "Pausing node2"
-  docker stop e2e-tendermint-2-1
-  sleep 10
-  docker start e2e-tendermint-2-1
-}
-
-# restart docker container tendermint-3-1 and restart it after 10s
-pause_node3() {
-  echo "Pausing node3"
-  docker stop e2e-tendermint-3-1
-  sleep 10
-  docker start e2e-tendermint-3-1
-}
+fun1
