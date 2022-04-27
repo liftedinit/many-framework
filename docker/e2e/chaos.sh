d20fd8933d2a86f7b46de350c4d74a44fea69a97
“moHam <mohamgoham@gmail.com>
2022-04-27 16:51:54 -0400

    address comments

--- docker/e2e/chaos.sh
+++ docker/e2e/chaos.sh
@@ -7,113 +7,112 @@ GREEN='\033[0;32m'
 
 c=0
 no_sleep () {
-    echo -e "${GREEN}RUN TENDERMINT NO SLEEP CHAOS TEST ${GREEN}"
+    echo "${GREEN}RUN TENDERMINT NO SLEEP CHAOS TEST ${GREEN}"
     while [ $c -le 14 ]
     do
-    echo "${RED}STOP TENDERMINT NODE 0 $c times${RED}"
-    ((c++))
-    docker stop e2e-tendermint-0-1 
-    echo "${GREEN}START TENDERMINT NODE 0 $c times${GREEN}"
-	  docker start e2e-tendermint-0-1
+      echo "${RED}STOP TENDERMINT NODE 0 $c times${RED}"
+      ((c++))
+      docker stop e2e-tendermint-0-1 
+      echo "${GREEN}START TENDERMINT NODE 0 $c times${GREEN}"
+	    docker start e2e-tendermint-0-1
 
-    echo "${RED}STOP TENDERMINT NODE 1  $c times${RED}"
-    docker stop e2e-tendermint-1-1 
-    echo "${GREEN}START TENDERMINT NODE 1  $c times${GREEN}"
-    docker start e2e-tendermint-1-1 
+      echo "${RED}STOP TENDERMINT NODE 1  $c times${RED}"
+      docker stop e2e-tendermint-1-1 
+      echo "${GREEN}START TENDERMINT NODE 1  $c times${GREEN}"
+      docker start e2e-tendermint-1-1 
 
-    echo "${RED}STOP TENDERMINT NODE 2  $c times${RED}"
-    docker stop e2e-tendermint-2-1 
-    echo "${GREEN}START TENDERMINT NODE 2  $c times${GREEN}"
-    docker start e2e-tendermint-2-1 
+      echo "${RED}STOP TENDERMINT NODE 2  $c times${RED}"
+      docker stop e2e-tendermint-2-1 
+      echo "${GREEN}START TENDERMINT NODE 2  $c times${GREEN}"
+      docker start e2e-tendermint-2-1 
 
-    echo "${RED}STOP TENDERMINT NODE 3  $c times${RED}"
-    docker stop e2e-tendermint-3-1 
-    echo "${GREEN}START TENDERMINT NODE 3  $c times${GREEN}"
-    docker start e2e-tendermint-3-1 
+      echo "${RED}STOP TENDERMINT NODE 3  $c times${RED}"
+      docker stop e2e-tendermint-3-1 
+      echo "${GREEN}START TENDERMINT NODE 3  $c times${GREEN}"
+      docker start e2e-tendermint-3-1 
 
-    echo "${NC}NO SLEEP TEST OVER${NC}"
+      echo "${NC}NO SLEEP TEST OVER${NC}"
 
-  done
+    done
     rapid_fire
 }
 
-
 rapid_fire () {
-    echo -e "${RED}RUN TENDERMINT RAPID FIRE CHAOS ${RED}"
+    echo "${RED}RUN TENDERMINT RAPID FIRE CHAOS ${RED}"
     while [ $c -le 30 ]
     do
-    echo "${RED}STOP TENDERMINT CHAOS TEST $c times${RED}"
-    ((c++))
-    echo "${RED}STOP TENDERMINT NODE 1${RED}"
-    docker stop e2e-tendermint-1-1 
-	  sleep 1
-    echo "${CYAN}START TENDERMINT NODE 1  $c times${CYAN}"
-	  docker start e2e-tendermint-1-1
-    sleep 1
+      echo "${RED}STOP TENDERMINT CHAOS TEST $c times${RED}"
+      ((c++))
+      echo "${RED}STOP TENDERMINT NODE 1${RED}"
+      docker stop e2e-tendermint-1-1 
+	    sleep 1
+      echo "${CYAN}START TENDERMINT NODE 1  $c times${CYAN}"
+	    docker start e2e-tendermint-1-1
+      sleep 1
 
-    echo "${CYAN}STOP TENDERMINT NODE 0  $c times${CYAN}"
-    docker stop e2e-tendermint-0-1 
-	  sleep 1
-    echo "${CYAN}START TENDERMINT NODE 0  $c times${CYAN}"
-    docker start e2e-tendermint-0-1 
-	  sleep 1
+      echo "${CYAN}STOP TENDERMINT NODE 0  $c times${CYAN}"
+      docker stop e2e-tendermint-0-1 
+	    sleep 1
+      echo "${CYAN}START TENDERMINT NODE 0  $c times${CYAN}"
+      docker start e2e-tendermint-0-1 
+	    sleep 1
 
-    echo "${CYAN}STOP TENDERMINT NODE 3  $c times${CYAN}"
-    docker stop e2e-tendermint-3-1 
-	  sleep 1
-    echo "${CYAN}START TENDERMINT NODE 3  $c times${CYAN}"
-    docker start e2e-tendermint-3-1 
-	  sleep 1
+      echo "${CYAN}STOP TENDERMINT NODE 3  $c times${CYAN}"
+      docker stop e2e-tendermint-3-1 
+	    sleep 1
+      echo "${CYAN}START TENDERMINT NODE 3  $c times${CYAN}"
+      docker start e2e-tendermint-3-1 
+	    sleep 1
 
-    echo "${CYAN}STOP TENDERMINT NODE 2  $c times${CYAN}"
-    docker stop e2e-tendermint-2-1 
-	  sleep 1
-    echo "${CYAN}Start TENDERMINT NODE 2  $c times${CYAN}"
-    docker start e2e-tendermint-2-1 
-	  sleep 1
+      echo "${CYAN}STOP TENDERMINT NODE 2  $c times${CYAN}"
+      docker stop e2e-tendermint-2-1 
+	    sleep 1
+      echo "${CYAN}Start TENDERMINT NODE 2  $c times${CYAN}"
+      docker start e2e-tendermint-2-1 
+	    sleep 1
 
-  done
+    done
     echo "${NC}RAPID FIRE TEST OVER${NC}"
     random_time
 }
 
 random_time () {
-    echo -e "${PURPLE}RUN TENDERMINT CHAOS TEST WITH RANDOM INTERVALS ${PURPLE}"
+    echo "${PURPLE}RUN TENDERMINT CHAOS TEST WITH RANDOM INTERVALS ${PURPLE}"
     while [ $c -le 45 ]
     do
-    echo "${RED}STOP TENDERMINT CHAOS TEST $c times${RED}"
-    ((c++))
+      echo "${RED}STOP TENDERMINT CHAOS TEST $c times${RED}"
+      ((c++))
+  
+      echo "${RED}STOP TENDERMINT NODE 1  $c times${RED}"
+      docker stop e2e-tendermint-1-1 
+	    sleep 2
+      echo "${PURPLE}START TENDERMINT NODE 1  $c times${PURPLE}"
+	    docker start e2e-tendermint-1-1
+      sleep 4
+  
+      echo "${RED}STOP TENDERMINT NODE 0  $c times${RED}"
+      docker stop e2e-tendermint-0-1 
+	    sleep 3
+      echo "${PURPLE}START TENDERMINT NODE 0  $c times${PURPLE}"
+      docker start e2e-tendermint-0-1 
+	    sleep 3
+  
+      echo "${RED}STOP TENDERMINT NODE 0  $c times${RED}"
+      docker stop e2e-tendermint-3-1 
+	    sleep 1
+      echo "${PURPLE}START TENDERMINT NODE 3  $c times${PURPLE}"
+      docker start e2e-tendermint-3-1 
+	    sleep 1
+  
+      echo "${RED}STOP TENDERMINT NODE 2  $c times${RED}"
+      docker stop e2e-tendermint-2-1 
+	    sleep 3
+      echo "${PURPLE}STOP TENDERMINT NODE 0  $c times${PURPLE}"
+      docker start e2e-tendermint-2-1 
+	    sleep 3
+      echo "${NC}RANDOM TIME TEST OVER${NC}"
 
-    echo "${RED}STOP TENDERMINT NODE 1  $c times${RED}"
-    docker stop e2e-tendermint-1-1 
-	  sleep 2
-    echo "${PURPLE}START TENDERMINT NODE 1  $c times${PURPLE}"
-	  docker start e2e-tendermint-1-1
-    sleep 4
-
-    echo "${RED}STOP TENDERMINT NODE 0  $c times${RED}"
-    docker stop e2e-tendermint-0-1 
-	  sleep 3
-    echo "${PURPLE}START TENDERMINT NODE 0  $c times${PURPLE}"
-    docker start e2e-tendermint-0-1 
-	  sleep 3
-
-    echo "${RED}STOP TENDERMINT NODE 0  $c times${RED}"
-    docker stop e2e-tendermint-3-1 
-	  sleep 1
-    echo "${PURPLE}START TENDERMINT NODE 3  $c times${PURPLE}"
-    docker start e2e-tendermint-3-1 
-	  sleep 1
-
-    echo "${RED}STOP TENDERMINT NODE 2  $c times${RED}"
-    docker stop e2e-tendermint-2-1 
-	  sleep 3
-    echo "${PURPLE}STOP TENDERMINT NODE 0  $c times${PURPLE}"
-    docker start e2e-tendermint-2-1 
-	  sleep 3
-    echo "${NC}RANDOM TIME TEST OVER${NC}"
-
-  done
+    done
     [[ "@" ]] && echo "options: $@"
 }
 
