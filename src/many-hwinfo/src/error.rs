38d0c5cb86b5a603756a24ea7291d8e65609e41f
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2022-04-27 15:06:58 -0400

    Remove many-hwinfo
    This is not used anymore

--- src/many-hwinfo/src/error.rs
+++
@@ -1,8 +0,0 @@
-use many::define_attribute_many_error;
-
-define_attribute_many_error!(
-    attribute 3 => {
-        1: pub fn unauthorized() => "Identity unauthorized to do this operation.",
-        5: pub fn invalid_initial_state(expected, actual)
-            => "Invalid initial state hash. Expected '{expected}', was '{actual}'.",    }
-);
