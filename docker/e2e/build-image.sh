bc9ab3421b883bed47ca41f02a5b4e2aa0ca4ed8
Yuri Albuquerque <yuridenommus@gmail.com>
2022-09-19 17:46:36 -0300

    Making sure it fails when a command fails

--- docker/e2e/build-image.sh
+++ docker/e2e/build-image.sh
@@ -1,5 +1,5 @@
-echo "filter-syscalls = false" >> /etc/nix/nix.conf
-nix build --max-jobs $CPUCORES $2
-cp $(readlink result) $1
-chown $UINFO $1
-rm result
+echo "filter-syscalls = false" >> /etc/nix/nix.conf &&
+    nix build --max-jobs $CPUCORES $2 &&
+    cp $(readlink result) $1 &&
+    chown $UINFO $1 &&
+    rm result
