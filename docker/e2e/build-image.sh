8fb2e2aa92d92866c29e209091c9e5be3f83952e
Yuri Albuquerque <yuridenommus@gmail.com>
2022-09-21 13:42:54 -0300

    Use a more stable nix image. Fixes #234

--- docker/e2e/build-image.sh
+++ docker/e2e/build-image.sh
@@ -1,6 +1,7 @@
 set -bepu
 
 echo "filter-syscalls = false" >> /etc/nix/nix.conf
+echo "experimental-features = nix-command flakes" >> /etc/nix/nix.conf
 nix build --max-jobs $CPUCORES $2
 cp $(readlink result) $1
 chown $UINFO $1
