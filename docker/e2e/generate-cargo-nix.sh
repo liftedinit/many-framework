0127cb7d2619a199333dae66737bd96458b1592a
Yuri Albuquerque <yuridenommus@gmail.com>
2022-09-22 12:04:03 -0300

    Improvements for the E2E Docker on MacOS (#239)
    * Improvements for the E2E Docker on MacOS
    
    Fixes #237
    
    * Update Cargo.nix
    
    * Always regenerate Cargo.nix
    
    * filter-syscalls = false on cargo2nix as well
    
    * Making sure it fails when a command fails
    
    * Run nproc over docker
    
    * Improve scripts
    
    * Use a more stable nix image. Fixes #234
    
    * Update Cargo.nix
    
    * Updating Cargo.nix now needs to be explicit

--- docker/e2e/generate-cargo-nix.sh
+++ docker/e2e/generate-cargo-nix.sh
@@ -1,3 +1,7 @@
+set -bepu
+
+echo "filter-syscalls = false" >> /etc/nix/nix.conf
+echo "experimental-features = nix-command flakes" >> /etc/nix/nix.conf
 nix run github:cargo2nix/cargo2nix --max-jobs $CPUCORES -- -f docker/e2e/Cargo.nix.new
 mv docker/e2e/Cargo.nix.new docker/e2e/Cargo.nix
 chown $UINFO docker/e2e/Cargo.nix
