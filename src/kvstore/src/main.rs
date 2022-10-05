8c7bc1e5b7e0954c06d1a1154e0bd5c9de06c042
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2022-10-05 11:26:32 -0400

    Use `syslog-tracing` crate instead of git repo

--- src/kvstore/src/main.rs
+++ src/kvstore/src/main.rs
@@ -305,7 +305,7 @@ fn main() {
         LogStrategy::Syslog => {
             let identity = std::ffi::CStr::from_bytes_with_nul(b"kvstore\0").unwrap();
             let (options, facility) = Default::default();
-            let syslog = tracing_syslog::Syslog::new(identity, options, facility).unwrap();
+            let syslog = syslog_tracing::Syslog::new(identity, options, facility).unwrap();
 
             let subscriber = subscriber.with_writer(syslog);
             subscriber.init();
