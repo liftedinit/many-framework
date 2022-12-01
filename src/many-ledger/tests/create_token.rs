f9d5236ff97efdc626988eac2cc1813db7edfdbf
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2022-12-01 09:30:30 -0500

    Test cleanup

--- src/many-ledger/tests/create_token.rs
+++ src/many-ledger/tests/create_token.rs
@@ -11,7 +11,9 @@ use many_identity_dsa::ecdsa::generate_random_ecdsa_identity;
 use many_ledger::error;
 use many_modules::account;
 use many_modules::account::features::{FeatureInfo, FeatureSet};
-use many_modules::account::{AccountModuleBackend, CreateArgs, Role};
+use many_modules::account::{
+    AccountModuleBackend, AddRolesArgs, CreateArgs, RemoveRolesArgs, Role,
+};
 use many_modules::ledger::{LedgerTokensModuleBackend, TokenCreateArgs, TokenCreateReturns};
 use many_types::ledger::{LedgerTokensAddressMap, TokenAmount, TokenMaybeOwner};
 use std::path::Path;
@@ -27,11 +29,17 @@ struct CreateWorld {
 }
 
 #[derive(Debug, Default, Eq, Parameter, PartialEq)]
-#[param(name = "id", regex = "(myself)|id ([0-9])")]
+#[param(
+    name = "id",
+    regex = "(myself)|id ([0-9])|(random)|(anonymous)|(the account)"
+)]
 enum SomeId {
     Id(u32),
     #[default]
     Myself,
+    Anonymous,
+    Random,
+    Account,
 }
 
 impl FromStr for SomeId {
@@ -40,6 +48,9 @@ impl FromStr for SomeId {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         Ok(match s {
             "myself" => Self::Myself,
+            "anonymous" => Self::Anonymous,
+            "random" => Self::Random,
+            "the account" => Self::Account,
             id => Self::Id(id.parse().expect("Unable to parse identity id")),
         })
     }
@@ -64,23 +75,54 @@ impl FromStr for SomeError {
         })
     }
 }
+#[derive(Debug, Default, Eq, Parameter, PartialEq)]
+#[param(name = "permission", regex = "(token creation)|(token mint)")]
+enum SomePermission {
+    #[default]
+    Create,
+    Mint,
+}
 
-// TODO: Generalize
-fn create_token_account_with(
-    w: &mut CreateWorld,
-    id: &Address,
-    roles: Option<BTreeMap<Address, BTreeSet<Role>>>,
-) -> Address {
-    let args = CreateArgs {
-        description: Some("Account Description".into()),
-        roles,
-        features: FeatureSet::from_iter([
-            account::features::tokens::TokenAccountLedger.as_feature()
-        ]),
-    };
-    AccountModuleBackend::create(&mut w.setup.module_impl, id, args)
-        .expect("Unable to create account")
-        .id
+impl FromStr for SomePermission {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "token creation" => Self::Create,
+            "token mint" => Self::Mint,
+            invalid => return Err(format!("Invalid `SomeError`: {invalid}")),
+        })
+    }
+}
+
+impl SomeId {
+    fn as_address(&self, w: &mut CreateWorld) -> Address {
+        match self {
+            SomeId::Myself => w.setup.id,
+            SomeId::Id(seed) => identity(*seed),
+            SomeId::Anonymous => Address::anonymous(),
+            SomeId::Random => generate_random_ecdsa_identity().address(),
+            SomeId::Account => w.account,
+        }
+    }
+}
+
+impl SomeError {
+    fn as_many(&self) -> ManyError {
+        match self {
+            SomeError::Unauthorized => error::unauthorized(),
+            SomeError::MissingPermission => account::errors::user_needs_role(Role::CanTokensCreate),
+        }
+    }
+}
+
+impl SomePermission {
+    fn as_role(&self) -> Role {
+        match self {
+            SomePermission::Create => Role::CanTokensCreate,
+            SomePermission::Mint => Role::CanTokensMint,
+        }
+    }
 }
 
 fn create_token(w: &mut CreateWorld, sender: &Address) {
@@ -110,21 +152,9 @@ fn given_token_decimals(w: &mut CreateWorld, decimals: u64) {
     w.args.summary.decimals = decimals;
 }
 
-#[given(expr = "myself as owner")]
-fn given_token_owner(w: &mut CreateWorld) {
-    w.args.owner = Some(TokenMaybeOwner::Left(w.setup.id));
-}
-
-#[given(expr = "a random owner")]
-fn given_random_owner(w: &mut CreateWorld) {
-    w.args.owner = Some(TokenMaybeOwner::Left(
-        generate_random_ecdsa_identity().address(),
-    ));
-}
-
-#[given(expr = "an anonymous owner")]
-fn given_anon_owner(w: &mut CreateWorld) {
-    w.args.owner = Some(TokenMaybeOwner::Left(Address::anonymous()));
+#[given(expr = "{id} as owner")]
+fn given_token_owner(w: &mut CreateWorld, id: SomeId) {
+    w.args.owner = Some(TokenMaybeOwner::Left(id.as_address(w)));
 }
 
 #[given(expr = "no owner")]
@@ -146,29 +176,61 @@ fn given_initial_distribution(w: &mut CreateWorld, id: u32, amount: u64) {
     distribution.insert(identity(id), TokenAmount::from(amount));
 }
 
-#[given(expr = "a token account where id {int} is the owner")]
-fn given_account_not_part_of(w: &mut CreateWorld, seed: u32) {
-    w.account = create_token_account_with(w, &identity(seed), None);
+#[given(expr = "a token account")]
+fn given_token_account(w: &mut CreateWorld) {
+    let account = AccountModuleBackend::create(
+        &mut w.setup.module_impl,
+        &w.setup.id,
+        CreateArgs {
+            description: Some("Token Account".into()),
+            features: FeatureSet::from_iter([
+                account::features::tokens::TokenAccountLedger.as_feature()
+            ]),
+            ..Default::default()
+        },
+    )
+    .expect("Unable to create account");
+    w.account = account.id
 }
 
-#[given(expr = "a token account I'm part of as Owner")]
-fn given_account_part_of_owner(w: &mut CreateWorld) {
-    let id = w.setup.id;
-    w.account = create_token_account_with(w, &id, None);
+#[given(expr = "{id} as the account owner")]
+fn given_account_id_owner(w: &mut CreateWorld, id: SomeId) {
+    let id = id.as_address(w);
+    AccountModuleBackend::add_roles(
+        &mut w.setup.module_impl,
+        &w.setup.id,
+        AddRolesArgs {
+            account: w.account,
+            roles: BTreeMap::from_iter([(id, BTreeSet::from([Role::Owner]))]),
+        },
+    )
+    .expect("Unable to add role to account");
+
+    if id != w.setup.id {
+        AccountModuleBackend::remove_roles(
+            &mut w.setup.module_impl,
+            &w.setup.id,
+            RemoveRolesArgs {
+                account: w.account,
+                roles: BTreeMap::from_iter([(w.setup.id, BTreeSet::from([Role::Owner]))]),
+            },
+        )
+        .expect("Unable to remove myself as account owner");
+    }
 }
 
-#[given(expr = "a token account id {int} is part of {word} token creation permission")]
-fn given_account_part_of_can_create(w: &mut CreateWorld, seed: u32, maybe_perm: String) {
-    let roles = match maybe_perm.as_str() {
-        "with" => Some(BTreeMap::from([(
-            identity(seed),
-            BTreeSet::from_iter([Role::CanTokensCreate]),
-        )])),
-        "without" => None,
-        _ => panic!("Invalid {maybe_perm}. Expected 'with' or 'without'"),
-    };
-    let id = w.setup.id;
-    w.account = create_token_account_with(w, &id, roles);
+#[given(expr = "{id} has {permission} permission")]
+fn given_account_part_of_can_create(w: &mut CreateWorld, id: SomeId, permission: SomePermission) {
+    let id = id.as_address(w);
+    AccountModuleBackend::add_roles(
+        &mut w.setup.module_impl,
+        &w.setup.id,
+        AddRolesArgs {
+            account: w.account,
+            roles: BTreeMap::from([(id, BTreeSet::from_iter([permission.as_role()]))]),
+        },
+    )
+    .expect("Unable to add role to account");
 }
 
 #[given(expr = "setting the account as the owner")]
@@ -176,29 +238,20 @@ fn given_account_owner(w: &mut CreateWorld) {
     w.args.owner = Some(TokenMaybeOwner::Left(w.account));
 }
 
-#[when(expr = "the token is created")]
-fn when_create_token(w: &mut CreateWorld) {
-    create_token(w, &w.setup.id.clone());
-}
-
-#[when(expr = "the token is created as id {int}")]
-fn when_create_token_as_id(w: &mut CreateWorld, id: u32) {
-    create_token(w, &identity(id));
+#[when(expr = "the token is created as {id}")]
+fn when_create_token(w: &mut CreateWorld, id: SomeId) {
+    let id = id.as_address(w);
+    create_token(w, &id);
 }
 
 #[then(expr = "creating the token as {id} fails with {error}")]
 fn then_create_token_fail_acl(w: &mut CreateWorld, id: SomeId, error: SomeError) {
-    let id = match id {
-        SomeId::Myself => w.setup.id,
-        SomeId::Id(seed) => identity(seed),
-    };
-
-    let error = match error {
-        SomeError::Unauthorized => error::unauthorized(),
-        SomeError::MissingPermission => account::errors::user_needs_role(Role::CanTokensCreate),
-    };
+    let id = id.as_address(w);
     fail_create_token(w, &id);
-    assert_eq!(w.error.as_ref().expect("Expecting an error"), &error);
+    assert_eq!(
+        w.error.as_ref().expect("Expecting an error"),
+        &error.as_many()
+    );
 }
 
 #[then(expr = "the token symbol is a subresource")]
@@ -216,19 +269,9 @@ fn then_token_name(w: &mut CreateWorld, name: String) {
     assert_eq!(w.result.info.summary.name, name);
 }
 
-#[then(expr = "the token owner is myself")]
-fn then_token_owner(w: &mut CreateWorld) {
-    assert_eq!(w.setup.id, w.result.info.owner.unwrap())
-}
-
-#[then(expr = "the token owner is the account")]
-fn then_token_owner_account(w: &mut CreateWorld) {
-    assert_eq!(w.account, w.result.info.owner.unwrap())
-}
-
-#[then(expr = "the sender is the owner")]
-fn then_token_no_owner(w: &mut CreateWorld) {
-    assert_eq!(w.result.info.owner, w.setup.id);
+#[then(expr = "the token owner is {id}")]
+fn then_token_owner(w: &mut CreateWorld, id: SomeId) {
+    assert_eq!(id.as_address(w), w.result.info.owner.unwrap())
 }
 
 #[then(expr = "the owner is removed")]
--- src/many-ledger/tests/features/ledger_tokens/create_token.feature
+++ src/many-ledger/tests/features/ledger_tokens/create_token.feature
@@ -8,7 +8,7 @@ Scenario: Creating a new token as myself
 	And myself as owner
 	Given id 1 has 123 initial tokens
 	And id 2 has 456 initial tokens
-	When the token is created
+	When the token is created as myself
 	Then the token symbol is a subresource
 	And the token ticker is FBR
 	And the token name is Foobar
@@ -19,37 +19,41 @@ Scenario: Creating a new token as myself
 
 @tokens
 Scenario: Creating a new token as someone random
-	Given a random owner
+	Given random as owner
 	Then creating the token as myself fails with unauthorized
 
 @tokens
 Scenario: Creating a new token as anonymous
-	Given an anonymous owner
+	Given anonymous as owner
 	Then creating the token as myself fails with unauthorized
 
 @tokens
 Scenario: Creating a new token on behalf of an account I'm not part of
-	Given a token account where id 5 is the owner
+	Given a token account
+	And id 5 as the account owner
 	And setting the account as the owner
 	Then creating the token as myself fails with missing permission
 
 @tokens
 Scenario: Creating a new token on behalf of an account I'm the Owner of
-	Given a token account I'm part of as Owner
+	Given a token account
+	And myself as the account owner
 	And setting the account as the owner
-	When the token is created
+	When the token is created as myself
 	Then the token owner is the account
 
 @tokens
 Scenario: Creating a new token on behalf of an account I'm part of with token creation permission
-	Given a token account id 5 is part of with token creation permission
+	Given a token account
+	And id 5 has token creation permission
 	And setting the account as the owner
 	When the token is created as id 5
 	Then the token owner is the account
 
 @tokens
 Scenario: Creating a new token on behalf of an account I'm part of without token creation permission
-	Given a token account id 6 is part of without token creation permission
+	Given a token account
+	And id 6 has token mint permission
 	And setting the account as the owner
 	Then creating the token as id 6 fails with missing permission
 
@@ -57,14 +61,14 @@ Scenario: Creating a new token on behalf of an account I'm part of without token
 Scenario: Creating a new token without owner (owner is sender)
 	Given a ticker FOO
 	And no owner
-	When the token is created
+	When the token is created as myself
 	Then the token ticker is FOO
-	And the sender is the owner
+	And the token owner is myself
 
 @tokens
 Scenario: Creating a new token, removing the owner
 	Given a ticker FOO
 	And removing the owner
-	When the token is created
+	When the token is created as myself
 	Then the token ticker is FOO
 	And the owner is removed
